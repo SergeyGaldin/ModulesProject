acdbda2687e9e2fff9abf0eb9b7608251158f50c
SergeyGaldin <galdinsd@cdisoft.ru>
2023-06-07 18:16:49 +0300

    добавлен новый модуль

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,14 +2,26 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.activity1.Activity1;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btnActivity1).setOnClickListener(view -> {
+            startActivity(new Intent(this, Activity1.class));
+        });
 
+        findViewById(R.id.btnActivity2).setOnClickListener(view -> {
+
+        });
     }
 }
